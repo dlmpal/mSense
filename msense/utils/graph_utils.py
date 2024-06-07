d5e5edaea7f715a9a50af5488a36c9469f71adc3
Dimitris Pallas <mc19054@mail.ntua.gr>
2024-06-07 21:38:29 +0300

    Added separate_local_global

--- msense/utils/graph_utils.py
+++ msense/utils/graph_utils.py
@@ -1,4 +1,4 @@
-from typing import Dict, List
+from typing import Dict, List, Tuple
 
 from msense.core.variable import Variable
 from msense.core.discipline import Discipline
@@ -25,3 +25,36 @@ def get_couplings(disciplines: List[Discipline]) -> List[Variable]:
             coupling_vars.append(out_var)
 
     return coupling_vars
+
+
+def separate_local_global(vars: List[Variable], disciplines: List[Discipline]) -> Tuple[Dict[Discipline, List[Variable]], List[Variable]]:
+    """
+    Separate local and global/shared variables, for a set of disciplines.
+    A variable is considered local if it enters only 1 discipline, else it is global.
+    """
+
+    # Map each variable to one or more disciplines
+    var_to_disc = {}
+    for var in vars:
+        var_to_disc[var] = []
+        for disc in disciplines:
+            if var in disc.input_vars:
+                var_to_disc[var].append(disc)
+
+    # Store the local variables for each discipline
+    local_vars = {}
+    for disc in disciplines:
+        local_vars[disc] = []
+
+    # Store the global varialbes in a contiguous list
+    global_vars = []
+
+    # Separate
+    for var in var_to_disc:
+        if len(var_to_disc[var]) == 1:
+            disc = var_to_disc[var][0]
+            local_vars[disc].append(var)
+        else:
+            global_vars.append(var)
+
+    return local_vars, global_vars
