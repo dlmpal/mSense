a503a8f664bb5dae85e18a179248f73f43fadcee
Dimitris Pallas <mc19054@mail.ntua.gr>
2024-06-13 16:33:56 +0300

    Restructuring-Enabled different types of eq. cons

--- msense/opt/problems/idf.py
+++ msense/opt/problems/idf.py
@@ -1,7 +1,9 @@
-from typing import List
+from typing import Dict, List
 
+from numpy import ndarray, ones
 from numpy.linalg import norm
 
+from msense.core.constants import FLOAT_DTYPE
 from msense.core.variable import Variable
 from msense.core.discipline import Discipline
 from msense.utils.graph_utils import get_couplings
@@ -13,8 +15,10 @@ class IDF(OptProblem):
     The Individual Discipline Feasible (IDF) approach for MDO problems.
     """
 
-    def __init__(self, disciplines: List[Discipline], feasibility_tol: float = 1e-6, **kwargs) -> None:
+    def __init__(self, disciplines: List[Discipline], scalar_feasibility_constraints: bool = False,
+                 feasibility_tol: float = 0.0, **kwargs) -> None:
         self.disciplines = disciplines
+        self.scalar_feasiblity_constraints = scalar_feasibility_constraints
 
         # Get the coupling variables and add them
         # to the design variables
@@ -22,14 +26,43 @@ class IDF(OptProblem):
         kwargs["design_vars"] += self.coupling_vars
 
         # Create feasibility/consistency constraints
-        self.feasibility_constraints = [
-            Variable(var.name + "_con", lb=0, ub=feasibility_tol) for var in self.coupling_vars]
+        self.feasibility_constraints = []
+        for var in self.coupling_vars:
+            con_size = 1 if self.scalar_feasiblity_constraints else var.size
+            self.feasibility_constraints.append(Variable(var.name + "_con", con_size, lb=-
+                                                         feasibility_tol, ub=feasibility_tol))
         if kwargs["constraints"] is None:
             kwargs["constraints"] = self.feasibility_constraints
         else:
             kwargs["constraints"] += self.feasibility_constraints
 
         super().__init__(**kwargs)
+
+    def _compute_feasibility_constraint(self, var: Variable, disc_outputs: Dict[str, ndarray]) -> ndarray:
+        con_value = None
+        if self.scalar_feasiblity_constraints:
+            con_value = norm(
+                self._values[var.name] - disc_outputs[var.name])**2
+        else:
+            con_value = self._values[var.name] - disc_outputs[var.name]
+        return con_value
+
+    def _compute_feasibility_constraint_jac(self, in_var: Variable, out_var: Variable,
+                                            disc_outputs: Dict[str, ndarray], disc_partials: Dict[str, Dict[str, ndarray]]) -> ndarray:
+        con_jac = None
+        if self.scalar_feasiblity_constraints:
+            if in_var.name == out_var.name:
+                con_jac = 2 * (self._values[out_var.name] -
+                               disc_outputs[out_var.name])
+            elif in_var.name in disc_partials[out_var.name]:
+                con_jac = -2 * (self._values[out_var.name] - disc_outputs[out_var.name]
+                                ) @ disc_partials[out_var.name][in_var.name]
+        else:
+            if in_var.name == out_var.name:
+                con_jac = ones((out_var.size, out_var.size), FLOAT_DTYPE)
+            elif in_var.name in disc_partials[out_var.name]:
+                con_jac = -disc_partials[out_var.name][in_var.name]
+        return con_jac
 
     def _eval(self):
         # Evaluate the disciplines
@@ -44,7 +77,7 @@ class IDF(OptProblem):
         # Evaluate feasibility constraints
         for var in self.coupling_vars:
             outputs[var.name +
-                    "_con"] = norm(self._values[var.name] - outputs[var.name])**2
+                    "_con"] = self._compute_feasibility_constraint(var, outputs)
 
         # Set the values of the constraints and the objective
         for var in self.output_vars:
@@ -61,13 +94,10 @@ class IDF(OptProblem):
         # Feasibility constraint jacobians
         for out_var in self.coupling_vars:
             for in_var in self.design_vars:
-                if in_var.name == out_var.name:
-                    self._jac[out_var.name + "_con"][out_var.name] = 2 * \
-                        (self._values[out_var.name] - outputs[out_var.name])
-                elif in_var.name in partials[out_var.name]:
-                    self._jac[out_var.name + "_con"][in_var.name] = -2 * \
-                        (self._values[out_var.name] - outputs[out_var.name]
-                         )  @ partials[out_var.name][in_var.name]
+                con_jac = self._compute_feasibility_constraint_jac(
+                    in_var, out_var, outputs, partials)
+                if con_jac is not None:
+                    self._jac[out_var.name + "_con"][in_var.name] = con_jac
 
         # Objective and constraint jacobians
         for out_var in self.output_vars:
