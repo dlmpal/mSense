964a5daf08050d5d51886e6549661f4b9b737265
Dimitris Pallas <mc19054@mail.ntua.gr>
2024-06-23 06:12:26 +0300

    Minor change

--- msense/opt/problems/co.py
+++ msense/opt/problems/co.py
@@ -1,5 +1,6 @@
 from typing import Dict, List, Tuple
 
+from numpy import ndarray
 from numpy import zeros
 from numpy.linalg import norm
 
@@ -22,12 +23,11 @@ class COSubProblem(OptProblem):
                  global_design_vars: List[Variable],
                  coupling_vars: List[Variable],
                  constraints: List[Variable],
-                 J: Variable):
+                 objective: Variable):
 
         self.disc = discipline
 
         self.local_design_vars = local_design_vars
-
         self.global_design_vars = []
         for var in global_design_vars:
             if var in self.disc.input_vars:
@@ -46,72 +46,78 @@ class COSubProblem(OptProblem):
             if var in self.disc.output_vars:
                 self.constraints.append(var)
 
-        # Consistency constraint
-        # This is the objective for the local optimization problem
-        self.J = J
-
-        # Values provided by the system-level optimizer
-        self.global_values = {}
-
         super().__init__(discipline.name + "SubProblem",
                          self.local_design_vars + self.global_design_vars,
-                         self.J,
+                         objective,
                          self.constraints)
+
+        # Values provided by the system-level optimizer
+        # These are set at the beginning
+        # of each system-level optimizer iteration
+        self._global_values = {}
+
+    def set_global_values(self, values: Dict[str, ndarray]):
+        self._global_values = copy_dict_1d(
+            self.local_design_vars + self.global_design_vars +
+            self.input_couplings + self.output_couplings, values)
 
     def _eval(self):
         # Gather the discipline inputs
-        disc_input_values = {}
+        local_values = {}
         for var in self.design_vars:
-            disc_input_values[var.name] = self._values[var.name].copy()
+            local_values[var.name] = self._values[var.name].copy()
         for var in self.input_couplings:
-            disc_input_values[var.name] = self.global_values[var.name].copy()
+            local_values[var.name] = self._global_values[var.name].copy()
 
         # Evaluate the discipline
-        disc_output_values = self.disc.eval(disc_input_values)
+        local_values.update(self.disc.eval(local_values))
 
         # Update the local constraints
         for var in self.constraints:
-            self._values[var.name] = disc_output_values[var.name]
+            self._values[var.name] = local_values[var.name]
 
-        # Evaluate the consistency constraint J
-        self._values[self.J.name] = zeros(self.J.size, FLOAT_DTYPE)
+        # Evaluate the feasibility constraint
+        self._values[self.objective.name] = zeros(
+            self.objective.size, FLOAT_DTYPE)
 
         for var in self.local_design_vars:
-            self._values[self.J.name] += norm(
-                self.global_values[var.name] - self._values[var.name])**2
+            self._values[self.objective.name] += norm(
+                self._global_values[var.name] - self._values[var.name])**2
 
         for var in self.global_design_vars:
-            self._values[self.J.name] += norm(
-                self._values[var.name] - self.global_values[var.name])**2
+            self._values[self.objective.name] += norm(
+                self._values[var.name] - self._global_values[var.name])**2
 
         for var in self.output_couplings:
-            self._values[self.J.name] += norm(
-                self.global_values[var.name] - disc_output_values[var.name])**2
+            self._values[self.objective.name] += norm(
+                self._global_values[var.name] - local_values[var.name])**2
 
     def _differentiate(self) -> None:
 
-        disc_output_values = self.disc.get_output_values()
+        disc_values = self.disc.get_values()
         disc_jac = self.disc.differentiate()
 
-        for var in self.local_design_vars:
-            self._jac[self.J.name][var.name] = -2 * \
-                (self.global_values[var.name] - self._values[var.name])
-            for cvar in self.output_couplings:
-                self._jac[self.J.name][var.name] += -2 * \
-                    (self.global_values[cvar.name] - disc_output_values[cvar.name]
-                     ) @ disc_jac[cvar.name][var.name]
+        for in_var in self.local_design_vars:
+            self._jac[self.objective.name][in_var.name] = -2 * \
+                (self._global_values[in_var.name] - self._values[in_var.name])
 
-        for var in self.global_design_vars:
-            self._jac[self.J.name][var.name] = 2 * \
-                (self._values[var.name] - self.global_values[var.name])
-            for cvar in self.output_couplings:
-                self._jac[self.J.name][var.name] += -2 * \
-                    (self.global_values[cvar.name] - disc_output_values[cvar.name]
-                     ) @ disc_jac[cvar.name][var.name]
+            for out_var in self.output_couplings:
+                self._jac[self.objective.name][in_var.name] += -2 * \
+                    (self._global_values[out_var.name] - disc_values[out_var.name]
+                     ) @ disc_jac[out_var.name][in_var.name]
+
+        for in_var in self.global_design_vars:
+            self._jac[self.objective.name][in_var.name] = 2 * \
+                (self._values[in_var.name] - self._global_values[in_var.name])
+
+            for out_var in self.output_couplings:
+                self._jac[self.objective.name][in_var.name] += -2 * \
+                    (self._global_values[out_var.name] - disc_values[out_var.name]
+                     ) @ disc_jac[out_var.name][in_var.name]
 
         for con in self.constraints:
-            for var in self.design_vars:
-                self._jac[con.name][var.name] = disc_jac[con.name][var.name]
+            for in_var in self.design_vars:
+                self._jac[con.name][in_var.name] = disc_jac[con.name][in_var.name]
 
 
 class CO(OptProblem):
@@ -119,39 +125,43 @@ class CO(OptProblem):
     The Collaborative Optimization (CO) approach for MDO problems.
     """
 
-    def __init__(self, disciplines: List[Discipline], **kwargs) -> None:
+    def __init__(self, disciplines: List[Discipline], warm_start: bool = False,
+                 feasibility_tol: float = 0.0, **kwargs) -> None:
         self.disciplines = disciplines[:-1]
         self.objective_discipline = disciplines[-1]
+        self.warm_start = warm_start
 
         # Get the coupling variables
         self.coupling_vars = get_couplings(self.disciplines)
-
-        # Separate the local and global constraints
-        if kwargs["constraints"] is None:
-            kwargs["constraints"] = []
-        self.global_constraints = []  # Global constraints
-        for var in kwargs["constraints"]:
-            if var in self.objective_discipline.output_vars:
-                self.global_constraints.append(var)
 
         # Separate the local and global design variables
         design_vars = kwargs["design_vars"]
         self.local_design_vars, self.global_design_vars = separate_local_global(
             design_vars, self.disciplines)
 
-        # Create the subproblems and the feasibility constraints J
+        # Separate the local and global constraints
+        if kwargs["constraints"] is None:
+            kwargs["constraints"] = []
+        self.global_constraints = []
+        for var in kwargs["constraints"]:
+            if var in self.objective_discipline.output_vars:
+                self.global_constraints.append(var)
+
+        # Create the subproblems and
+        # the disciplinary feasibility constraints
         self.subproblems: List[COSubProblem] = []
         self.feasibility_constraints = []
         for disc in self.disciplines:
-            J = Variable("J_" + disc.name, lb=0, ub=0)
+            con = Variable(disc.name + "_con",
+                           lb=-feasibility_tol, ub=feasibility_tol)
             subprob = COSubProblem(disc,
                                    self.local_design_vars[disc],
                                    self.global_design_vars,
                                    self.coupling_vars,
                                    kwargs["constraints"],
-                                   J)
+                                   con)
             self.subproblems.append(subprob)
-            self.feasibility_constraints.append(J)
+            self.feasibility_constraints.append(con)
 
         # Initialize the base OptProblem
         kwargs["design_vars"] += self.coupling_vars
@@ -160,21 +170,14 @@ class CO(OptProblem):
         super().__init__(**kwargs)
 
     def _eval(self) -> None:
-
         # Solve the subproblems
-        # Get the values of the consistency constraints
+        # Get the values of the feasibility constraints
         for subprob in self.subproblems:
-            subprob.global_values = copy_dict_1d(
-                subprob.local_design_vars +
-                subprob.global_design_vars +
-                subprob.input_couplings +
-                subprob.output_couplings, self._values)
-
-            subprob_design_vec = copy_dict_1d(
-                subprob.local_design_vars + subprob.global_design_vars, self._values)
-
-            subprob.solve(subprob_design_vec)
-
+            subprob.set_global_values(self._values)
+            if self.driver.iter == 0 or self.warm_start is False:
+                subprob.solve(copy_dict_1d(subprob.design_vars, self._values))
+            else:
+                subprob.solve(subprob.get_input_values())
             self._values[subprob.objective.name] = subprob.get_output_values()[
                 subprob.objective.name]
 
@@ -183,3 +186,29 @@ class CO(OptProblem):
         self._values[self.objective.name] = obj_disc_values[self.objective.name]
         for var in self.global_constraints:
             self._values[var.name] = obj_disc_values[var.name]
+
+    def _differentiate(self) -> None:
+        # Differentiate the feasibility constraints
+        # This is done according to the post-optimal sensitivity results
+        # of Braun
+        for subprob in self.subproblems:
+            values = subprob._global_values
+            opt_values = subprob.disc.get_values()
+
+            for var in subprob.local_design_vars:
+                self._jac[subprob.objective.name][var.name] = -2 * \
+                    (values[var.name] - opt_values[var.name])
+
+            for var in subprob.global_design_vars:
+                self._jac[subprob.objective.name][var.name] = 2 * \
+                    (values[var.name] - opt_values[var.name])
+
+            for var in subprob.output_couplings:
+                self._jac[subprob.objective.name][var.name] = 2 * \
+                    (values[var.name] - opt_values[var.name])
+
+        # Differentiate the objective discipline
+        obj_disc_jac = self.objective_discipline.differentiate()
+        for out_var in self.objective_discipline.output_vars:
+            for in_var in self.objective_discipline.input_vars:
+                self._jac[out_var.name][in_var.name] = obj_disc_jac[out_var.name][in_var.name]
