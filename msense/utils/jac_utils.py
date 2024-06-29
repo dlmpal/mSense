79ec3c2d9de3f915c28c0819204374e8bf2cf690
Dimitris Pallas <mc19054@mail.ntua.gr>
2024-06-30 01:10:47 +0300

    Added scaling to FD step size

--- msense/utils/jac_utils.py
+++ msense/utils/jac_utils.py
@@ -20,23 +20,37 @@ def finite_difference_approx(func: Callable[[Dict[str, ndarray]], Dict[str, ndar
                              dinput_vars: List[Variable], doutput_vars: List[Variable],
                              input_values: Dict[str, ndarray], output_values: Dict[str, ndarray] = None,
                              jac: Dict[str, Dict[str, ndarray]] = None, eps=1e-6) -> Dict[str, Dict[str, ndarray]]:
+    """
+    Compute the jacobian of func using Finite-Differences
+    """
+    # Evaluate the function if the output values are not provided
     if output_values is None:
         output_values = func(input_values)
+
+    # Initialize the jacobian, if one is not provided
     if jac is None:
         jac = initialize_dense_jac(dinput_vars, doutput_vars)
+
+    # Loop over the input variables
     for in_var in dinput_vars:
         for i in range(in_var.size):
-            # Perturb the i_th entry of the input variable
-            temp = input_values[in_var.name][i].copy()
-            input_values[in_var.name][i] += eps
-            # Run the function with the perturbed input values
+            # Compute the perturbation
+            dx = eps * (1 + abs(input_values[in_var.name][i]))
+
+            # Perturb the input variable
+            input_values[in_var.name][i] += dx
+
+            # Evaluate the function with the perturbed input values
             output_values_p = func(input_values)
+
             # Compute the jacobian entry
             for out_var in doutput_vars:
                 jac[out_var.name][in_var.name][:, i] = (
-                    output_values_p[out_var.name] - output_values[out_var.name]) / eps
+                    output_values_p[out_var.name] - output_values[out_var.name]) / dx
+
             # Reset the perturbed value
-            input_values[in_var.name][i] = temp
+            input_values[in_var.name][i] -= dx
+
     return jac
 
 
@@ -44,24 +58,34 @@ def complex_step_approx(func: Callable[[Dict[str, ndarray]], Dict[str, ndarray]]
                         dinput_vars: List[Variable], doutput_vars: List[Variable],
                         input_values: Dict[str, ndarray],
                         jac: Dict[str, Dict[str, ndarray]], eps=1e-6) -> Dict[str, Dict[str, ndarray]]:
+    """
+    Compute the jacobian of func using the Complex-Step method
+    """
     # Cast the input values to complex
     for in_var_name in input_values.keys():
         input_values[in_var_name] = input_values[in_var_name].astype(
             COMPLEX_DTYPE)
+
     # If required, initialize jac
     if jac is None:
         jac = initialize_dense_jac(dinput_vars, doutput_vars)
+
+    # Loop over the input variables
     for in_var in dinput_vars:
         for i in range(in_var.size):
-            # Perturb the i_th entry of the input variable
-            temp = input_values[in_var.name][i].copy()
+
+            # Perturb the input variable
             input_values[in_var.name][i] += eps * 1j
-            # Run the function with the perturbed input values
+
+            # Evaluate the function with the perturbed input values
             output_values_p = func(input_values)
+
             # Compute the jacobian entry
             for out_var in doutput_vars:
                 jac[out_var.name][in_var.name][:, i] = imag(
                     output_values_p[out_var.name]) / eps
+
             # Reset the perturbed value
-            input_values[in_var.name][i] = temp
+            input_values[in_var.name][i] -= eps * 1j
+
     return jac
