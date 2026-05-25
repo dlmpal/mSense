ecf49773626f2b3b206ab97b0861452a58e5db31
Dimitris Pallas <mc19054@mail.ntua.gr>
2026-05-25 18:01:21 +0300

    Minor changes

--- msense/core/constants.py
+++ msense/core/constants.py
@@ -3,6 +3,6 @@ from numpy import complex128
 
 # Datatype for floating-point arithmetic
 FLOAT_DTYPE = float64
-"""  """
+
 # Datatype for complex floating-point arithmetic
 COMPLEX_DTYPE = complex128
